9cf204839e637ba833238cc0782713fdd354f6db
John Doe <hamedhasan422421@gmail.com>
2022-07-28 22:13:27 +0300

    implement initial state & reducer

---
+++ src/context/initialState.js
@@ -0,0 +1,12 @@
+const { fetchUser, fetchCart } = require("../utils/fetchLocalStorageData");
+
+
+const userInfo = fetchUser();
+const cartInfo = fetchCart();
+
+export const initialState = {
+    user: userInfo,
+    foodItems: null,
+    cartShow: false,
+    cartItems: cartInfo,
+}
