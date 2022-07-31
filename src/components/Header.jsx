1dc244f130af8f2ed3ceaa02b8086a9398dc6802
John Doe <hamedhasan422421@gmail.com>
2022-07-31 20:54:50 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,14 +12,8 @@ import { useStateValue } from '../context/StateProvider'
 
 const Header = () => {
 
-  const [{ user, cartShow, cartItems }, dispatch] = useStateValue();
+ 
 
-  const showCart = () => {
-    // dispatch({
-    //   type: actionType.SET_CART_SHOW,
-    //   cartShow: !cartShow,
-    // });
-  };
 
     return (
         <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
@@ -43,26 +37,17 @@ className='flex items-center gap-24'
 <li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Services</li>
 </motion.ul>
 
-<div className="relative flex items-center justify-center"
-            onClick={showCart}
-          >
-            <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
-            {cartItems && cartItems.length > 0 && (
-              <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
-                <p className="text-xs text-white font-semibold">
-                  {cartItems.length}
-                </p>
-              </div>
-            )}
-          </div>
-          <div className='relative'>
-              <motion.img whileTap={{ scale: 0.6 }}
-              src={user ? user.photoURL : Avatar}
-              />
-          </div>
+<div className="relative flex items-center justify-center">
+<MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
 
+<div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+<p className="text-xs text-white font-semibold">
+        2
+   </p>
+</div>
 </div>
 
+ </div>
           </div>
 
         </header>
