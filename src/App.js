ff018f649c5c545eea6c0e1fe2268ed2956d2792
John Doe <hamedhasan422421@gmail.com>
2022-07-31 21:18:25 +0300

    update few route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
 import Header from './components/Header';
-
+import { Route, Routes } from "react-router-dom"
+import MainContainer from './components/MainContainer';
+import CreateContainer from './components/CreateContainer';
 function App() {
   return (
   <div className="w-screen h-auto flex flex-col bg-primary">
     <Header/>
  
+    <main className='mt-14 md:mt-20 px-4 md:px-16 py-4 w-full'>
+
+      <Routes>
+          <Route path="/*" element={<MainContainer/>} />
+          <Route path="/createItem" element={<CreateContainer/>} />
+      </Routes>
+    </main>
   </div>
   );
 }
