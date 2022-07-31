e367aea9770a650b156387e089495f28fe25fa18
John Doe <hamedhasan422421@gmail.com>
2022-07-31 20:01:28 +0300

    update on header li

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,8 +14,20 @@ const Header = () => {
           <img src={Logo} className='w-8 object-cover' alt="logo" />
           <p className='text-headingColor text-xl font-bold'>City</p>
         </a>
+<div className='flex items-center gap-24'>
+<motion.ul 
+initial={{ opacity: 0, x: 200 }}
+animate={{ opacity: 1, x: 0 }}
+exit={{ opacity: 0, x: 200}}
+className='flex items-center gap-24'
+>
+<li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
+<li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
+<li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
+<li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Services</li>
+</motion.ul>
+</div>
 
-        
           </div>
 
         </header>
