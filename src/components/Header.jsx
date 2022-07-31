a8081c0ea518b4b43931351942d16e430ba0d0b8
John Doe <hamedhasan422421@gmail.com>
2022-07-31 20:17:17 +0300

    update on useState value

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,14 @@ import { MdShoppingBasket } from 'react-icons/md'
 import { Link } from 'react-router-dom';
 import Logo from '../img/logo.png'
 
+import { useStateValue } from '../context/StateProvider'
 
 
 
 
 const Header = () => {
 
-
+const [{ user, cartShow, cartItems }, dispatch] = useStateValue();
 
   const showCart = () => {
     dispatch({
@@ -42,11 +43,18 @@ className='flex items-center gap-24'
 <li className='text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Services</li>
 </motion.ul>
 
-<div className='relative flex items-center justify-center'
-onClick={showCart}
->
-
-</div>
+<div className="relative flex items-center justify-center"
+            onClick={showCart}
+          >
+            <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
+            {cartItems && cartItems.length > 0 && (
+              <div className=" absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                <p className="text-xs text-white font-semibold">
+                  {cartItems.length}
+                </p>
+              </div>
+            )}
+          </div>
 
 </div>
 
