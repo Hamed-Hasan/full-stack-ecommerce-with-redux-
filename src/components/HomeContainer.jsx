6e19e76b19e00a75bdbad62a6345210d0e479374
John Doe <hamedhasan422421@gmail.com>
2022-08-01 23:16:46 +0300

    updating on home pages

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Delivery from '../img/delivery.png'
+import Delivery from '../img/delivery.png';
+import HeroBg from "../img/heroBg.png";
+import { heroData } from "../utils/data";
 const HomeContainer = () => {
     return (
         <section className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full' id="home">
@@ -34,7 +36,37 @@ const HomeContainer = () => {
           Order Now
         </button>
         </div>
-        
+        <div className='py-2 flex-1 flex items-center relative'>
+        <img src={HeroBg} alt="hero background"
+        className="ml-auto h-420 w-full lg:w-auto lg:h-650"
+        />
+         <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
+            {heroData && heroData.map((n) => (
+                <div key={n.id}
+                className='lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg   <img
+                  src={n.imageSrc}
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
+                  alt="I1"
+                />'
+                >
+                       <img
+                  src={n.imageSrc}
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20 "
+                  alt="I1"
+                />
+    <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {n.name}
+                </p>
+                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {n.decp}
+                </p>
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span> {n.price}
+                </p>
+                </div>
+            ))}
+         </div>
+        </div>
     </section>
     );
 };
