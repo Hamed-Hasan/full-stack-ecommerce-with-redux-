d7afca9c35b311c558ec0f5d7d3e350ac16d8da8
John Doe <hamedhasan422421@gmail.com>
2022-08-01 19:18:28 +0300

    update on header

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
-import { StateProvider } from "./context/StateProvider";
-import { initialState } from "./context/initialState";
-import reducer from "./context/reducer"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initalState";
+import reducer from "./context/reducer";
+
+ReactDOM.render(
   <Router>
     <StateProvider initialState={initialState} reducer={reducer}>
-    </StateProvider>
       <App />
+    </StateProvider>
   </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
