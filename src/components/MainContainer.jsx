51c10a1724ec9c19143d7c7b504c5b23087dc67c
John Doe <hamedhasan422421@gmail.com>
2022-08-01 19:45:14 +0300

    import react icons

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+    MdFastfood,
+    MdCloudUpload,
+    MdDelete,
+    MdFoodBank,
+    MdAttachMoney,
+  } from "react-icons/md";
+
+const MainContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MainContainer;
