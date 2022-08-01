4f2d05a788675a8974cd1f845368d816745e9221
John Doe <hamedhasan422421@gmail.com>
2022-08-01 21:08:54 +0300

    upload image added

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from "framer-motion";
-
+import Loader from "./Loader";
 import {
   MdFastfood,
   MdCloudUpload,
@@ -8,6 +8,13 @@ import {
   MdFoodBank,
   MdAttachMoney,
 } from "react-icons/md";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { storage } from "../firebase.config";
 import { useState } from 'react';
 import { useStateValue } from '../context/StateProvider';
 import {categories} from '../utils/data'
@@ -23,6 +30,43 @@ const CreateContainer = () => {
     const [msg, setMsg] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [{ foodItems }, dispatch] = useStateValue();
+
+    const uploadImage = (e) => {
+      setIsLoading(true);
+      const imageFile = e.target.files[0];
+      const storageRef = ref(storage, `Images/${Date.now()}-${imageFile.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, imageFile);
+  
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const uploadProgress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          console.log(error);
+          setFields(true);
+          setMsg("Error while uploading : Try AGain 🙇");
+          setAlertStatus("danger");
+          setTimeout(() => {
+            setFields(false);
+            setIsLoading(false);
+          }, 4000);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageAsset(downloadURL);
+            setIsLoading(false);
+            setFields(true);
+            setMsg("Image uploaded successfully 😊");
+            setAlertStatus("success");
+            setTimeout(() => {
+              setFields(false);
+            }, 4000);
+          });
+        }
+      );
+    };
 
     return (
   
@@ -76,7 +120,50 @@ const CreateContainer = () => {
               ))}
           </select>
         </div>
-
+        <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg">
+          {isLoading ? (
+            <Loader  />
+          ) : (
+            <>
+              {!imageAsset ? (
+                <>
+                  <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                    <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+                      <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+                      <p className="text-gray-500 hover:text-gray-700">
+                        Click here to upload
+                      </p>
+                    </div>
+                    <input
+                      type="file"
+                      name="uploadimage"
+                      accept="image/*"
+                      onChange={uploadImage}
+                      className="w-0 h-0"
+                    />
+                  </label>
+                </>
+              ) : (
+                <>
+                  <div className="relative h-full">
+                    <img
+                      src={imageAsset}
+                      alt="uploaded image"
+                      className="w-full h-full object-cover"
+                    />
+                    <button
+                      type="button"
+                      className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md  duration-500 transition-all ease-in-out"
+                    //   onClick={deleteImage}
+                    >
+                      <MdDelete className="text-white" />
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </div>
                 </div>
             </div>
 
