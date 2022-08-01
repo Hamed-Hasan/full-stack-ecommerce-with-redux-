b7adb8ae3f588217a0d81869f25455181936caff
John Doe <hamedhasan422421@gmail.com>
2022-08-01 23:03:15 +0300

    order now button added

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -27,6 +27,12 @@ const HomeContainer = () => {
           deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
           suscipit!
         </p>
+        <button
+          type="button"
+          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+        >
+          Order Now
+        </button>
         </div>
         
     </section>
