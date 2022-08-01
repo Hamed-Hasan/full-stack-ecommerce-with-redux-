729fd2bfdcb5897fc91122a17844f6933f584e8f
John Doe <hamedhasan422421@gmail.com>
2022-08-01 22:50:53 +0300

    adding home page button

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomeContainer from './HomeContainer';
 
 
 const MainContainer = () => {
     return (
         <div>
-            
+            <HomeContainer/>
         </div>
     );
 };
